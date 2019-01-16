7cdabb647595b723da7a6e5b11fadf8f826838a1
nandodoria <nandodoria@hotmail.ca>
2019-01-16 13:41:51 -0500

    working with routers

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 (() => {
 
-    const liveuser = {
-        props: ['first_name', 'last_name', 'role'],
+    const HomePageComponent = {
+        template: "<h2> You're on the home page </h2>"
 
-        template: "#activeuser",
-
-        methods: {
-            logChildMsg() {
-                console.log("hello from the child component");
-            },
-
-            runParentFunc() {
-                this.$emit('pass-func-call-up')
-            }
-        },
-
-        created: function() {
-            console.log('child component is live');
-        }
     };
+
+    const UsersPageComponent = {
+        template: "<h2> You're on the users page </h2>"
+    };
+
+    const routes = [
+        { path: '/', name: 'home', component: HomePageComponent },
+        { path: '/users', name: 'users', component: UsersPageComponent },
+    ];
+
+    const router = new VueRouter({
+        routes
+    });
 
 
     const vm = new Vue({
@@ -43,8 +41,11 @@
          },
 
          components: {
-            user: liveuser
-         }
+            'HomePageComponent': HomePageComponent,
+            'UsersPageComponent': UsersPageComponent
+         },
+
+         router: router
 
 
 
